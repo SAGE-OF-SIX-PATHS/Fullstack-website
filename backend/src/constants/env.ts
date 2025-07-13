ad4f8cfd4390f103dc705f825e4cba7cc29273d0
Akpamgbo Philip <nzubeakpamgbo>
2025-07-13 11:08:58 +0100

    fix port 3000 to 5000

--- backend/src/constants/env.ts
+++ backend/src/constants/env.ts
@@ -15,7 +15,7 @@ const getEnv = (key: string, defaultValue?: string): string => {
 };
 
 export const NODE_ENV = getEnv("NODE_ENV", "development");
-export const PORT = getEnv("PORT", "3000");
+export const PORT = getEnv("PORT", "5000");
 export const MONGO_URI = getEnv("MONGO_URI");
 export const JWT_SECRET = getEnv("JWT_SECRET");
 export const JWT_REFRESH_SECRET = getEnv("JWT_REFRESH_SECRET");
